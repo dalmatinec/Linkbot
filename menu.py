2596ebba9b5e82ca835435121a4db916b328eb7f
dalmatinec <temirbekov1602@gmail.com>
2026-05-24 17:25:54 +0500

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,70 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def main_menu():
+
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                text="Наш чат 💬",
+                callback_data="chat"
+            ),
+
+            InlineKeyboardButton(
+                text="Канал 📺",
+                callback_data="channel"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                text="Наш сайт-визитка 🌐",
+                callback_data="site"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                text="Резервный чат 🔐",
+                callback_data="reserve"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                text="Оператор сис 🤍",
+                callback_data="operator1"
+            ),
+
+            InlineKeyboardButton(
+                text="Оператор бро 💚",
+                callback_data="operator2"
+            )
+        ]
+    ]
+
+    return InlineKeyboardMarkup(
+        inline_keyboard=keyboard
+    )
+
+
+def confirm_broadcast_keyboard():
+
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                text="✅ Отправить",
+                callback_data="broadcast_yes"
+            ),
+
+            InlineKeyboardButton(
+                text="❌ Отмена",
+                callback_data="broadcast_no"
+            )
+        ]
+    ]
+
+    return InlineKeyboardMarkup(
+        inline_keyboard=keyboard
+    )
