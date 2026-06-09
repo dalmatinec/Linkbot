a27114da8869bb4ba84caf7d0f53e3811b366b34
dalmatinec <temirbekov1602@gmail.com>
2026-06-09 12:20:46 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,20 @@
-BOT_TOKEN = "8480271932:AAEzbh3s1XvXnQlIp-MD_qYIrCZ6Lmmlucg"
+# config.py
 
-OWNER_ID = 8658409677
+BOT_TOKEN = "8866817695:AAHfZovjYFeC60RVxbi7uukMrEkTBhdyq3I"
 
-LOG_CHANNEL_ID = -1003940647707
+OWNER_ID = 8243233601
 
-DB_PATH = "users.db"
+LOG_CHANNEL_ID = -1003922852212
+
+ADMIN_GROUP_ID = -1003555825169
+
+DATABASE_NAME = "database.db"
+
+BACKUP_DIR = "backups"
+
+# Фиксированные ссылки
+SITE_URL = "https://ladyshop.info"
+
+OPERATOR_SIS = "https://t.me/lady_4mmc"
+
+OPERATOR_BRO = "https://t.me/mens_420"
